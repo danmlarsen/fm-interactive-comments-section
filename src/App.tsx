47cd1094930015ce07a969d7a4223c8ba6205609
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-01-03 14:46:30 +0100

    Added fonts & colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <main className="grid min-h-screen place-items-center bg-slate-900 text-white">
+    <main className="grid min-h-screen place-items-center">
       <h1 className="text-7xl font-bold">Hello, World!</h1>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./indexh.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          100: "hsl(228, 33%, 97%)",
+          200: "hsl(223, 19%, 93%)",
+          300: "hsl(239, 57%, 85%)",
+          500: "hsl(211, 10%, 45%)",
+          800: "hsl(212, 24%, 26%)",
+        },
+        blue: "hsl(238, 40%, 52%)",
+        red: {
+          200: "hsl(358, 79%, 66%)",
+          500: "hsl(358, 79%, 66%)",
+        },
+      },
+      fontFamily: {
+        sans: ["Rubik", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };

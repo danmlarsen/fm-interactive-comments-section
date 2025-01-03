c5aa1f9e3b672677a4d760aa60f3ea32ff0e508a
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-01-03 14:18:30 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="grid min-h-screen place-items-center bg-slate-900 text-white">
+      <h1 className="text-7xl font-bold">Hello, World!</h1>
+    </main>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./indexh.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

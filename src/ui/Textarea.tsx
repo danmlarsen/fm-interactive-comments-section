bd9e53f9d495caa503eaca88eb1a84c5bf549f92
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-01-22 18:21:55 +0100

    Allow vertical resize

--- src/ui/Textarea.tsx
+++ src/ui/Textarea.tsx
@@ -8,7 +8,7 @@ export default function Textarea({
     <textarea
       rows={3}
       className={twMerge(
-        "focus-visible:ring-blue w-full resize-none rounded-lg border border-gray-200 px-6 py-3 ring-offset-transparent transition duration-300 placeholder:text-gray-500 focus:outline-none focus-visible:ring-1",
+        "w-full resize-y rounded-lg border border-gray-200 px-6 py-3 ring-offset-transparent transition duration-300 placeholder:text-gray-500 focus:outline-none focus-visible:ring-1 focus-visible:ring-blue",
         className,
       )}
       {...props}
